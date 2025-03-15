504de68d8d6274afea1a18362dd4bc099126b31d
unknown <ramkrushnsalunkhe99@gmail.com>
2025-03-15 16:19:48 +0530

    add the validation on vr report for invalid value

--- src/components/MeasurementOfVoltageRatio.jsx
+++ src/components/MeasurementOfVoltageRatio.jsx
@@ -47,13 +47,29 @@ export default function MeasurementOfVoltageRatio() {
         }));
     };
 
+    const CalculateAndSetErrorsValues = (tableName, rowName, columnName, newValue, e)=>{
+        const StandardValue = voltageRatioReportTableData[tableName][rowName]['Standard Ratio'];
+        const ErrorValue = (((newValue - StandardValue)/newValue) * 100).toFixed(4);
+        updateReportTableData(tableName, rowName, `% Error ${columnName}` ,ErrorValue);
+        // console.log(ErrorValue);
+        // console.log(e.target.classList);
+        console.log(e.currentTarget)
+        // e.target.classList.add("error-value");
+        if (ErrorValue > -0.5 && ErrorValue < 0.5) {
+            // console.log("Value is between -0.5 and 0.5");
+        } else {
+            e.target.style.color = "red";
+            // console.log("Value is outside the range");
+        }
+    }
+
     const OnReportDataChange = (e)=>{
         setIsAnyDataChange(true);
         const tableName = e.target.dataset.tableName;
         const columnName = e.target.name;
         const rowName = e.target.dataset.rowName;
         const newValue = e.target.value;
-
+        CalculateAndSetErrorsValues(tableName, rowName, columnName, newValue, e);
         updateReportTableData(tableName, rowName, columnName, newValue);
     }
 
@@ -106,9 +122,9 @@ export default function MeasurementOfVoltageRatio() {
                                                     ([key_i, value_i]) => (
                                                         <td key={key_i} title={`${key} ${key_i}`}>
                                                             {
-                                                                key_i.toLowerCase() === 'standard ratio' ? 
+                                                                key_i.toLowerCase() === 'standard ratio' || key_i.includes("% Error") ? 
                                                                 value_i :
-                                                                <input id={`${table_name}_${key}_${key_i}`} onFocus={(e)=>{activeInputRef.current=e.target.id}} type="number" value={value_i} data-row-name={key} data-table-name={table_name} name={key_i} onChange={OnReportDataChange}  />
+                                                                <input id={`${table_name}_${key}_${key_i}`} className={(value[`% Error ${key_i}`] > 0.5 || value[`% Error ${key_i}`] < -0.5) && "error-value"} onFocus={(e)=>{activeInputRef.current=e.target.id}} type="number" value={value_i} data-row-name={key} data-table-name={table_name} name={key_i} onChange={OnReportDataChange}  />
                                                             }
                                                         </td>
                                                     )
@@ -122,7 +138,7 @@ export default function MeasurementOfVoltageRatio() {
                     )
                 )}
             </div>
-
+            <p className="report-note">Interpretation: Since % error at all taps is less than ±0.5%, Test has passed. </p>
             <NextPreviousButton
                 isSaving={isSaving}
                 OnSaveClick={OnSaveClick}
